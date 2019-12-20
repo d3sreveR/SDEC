dfc79b042aa6ae286882f13a944e0c1cd043959a
Ivan <56585516+d3sreveR@users.noreply.github.com>
2019-12-20 02:18:25 +0100

    Arrays and For cyclus

---
+++ Arrays and For/script.js
@@ -0,0 +1,23 @@
+//Odd numbers
+
+let even = null;
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        even = i;
+        console.log(even)
+    }
+}
+
+//===================
+
+// Largest number in the array
+
+let array = [3, 6, 2, 56, 32, 5, 89, 32];
+let largest = 0;
+
+for (i = 0; i <= largest; i++) {
+    if (array[i] > largest) {
+        let largest = array[i];
+    }
+}
+console.log(largest)
