493f0a310807b12a9da8c11361be65b9e9911369
Ivan <56585516+d3sreveR@users.noreply.github.com>
2019-12-26 20:23:11 +0100

    Update script.js

--- Objects/script.js
+++ Objects/script.js
@@ -1,7 +1,23 @@
 animal = new Object ();
 animal.name = "Doggo";
-animal.kind = "German Shepherd"
+animal.kind = "German Shepherd";
 animal.speak = function () {
     return "hey bro!!!";
 }
 console.log(`Dog says ${animal.speak()}`)
+
+//============================\\
+
+book = new Object ();
+book.title = "The Robots of dawn";
+book.author = "Isaac Asimov";
+book.readingStatus = true;
+    checkreadingStatus = function () {
+    if(this.readingStatus === true) {
+       return "Already read 'The Robots of dawn' by Isaac Asimov.";
+    } else {
+        return "You still need to read 'Mockingjay: The Final Book of The Hunger Games' by Suzanne Collins. (if false).";
+    }
+}; 
+
+console.log(checkreadingStatus())
