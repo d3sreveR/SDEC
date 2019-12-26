bbf8e318e52ea7a483e7e298ca20eef0200457eb
Ivan <56585516+d3sreveR@users.noreply.github.com>
2019-12-26 20:10:12 +0100

    Update script.js

--- Objects/script.js
+++ Objects/script.js
@@ -2,7 +2,6 @@ animal = new Object ();
 animal.name = "Doggo";
 animal.kind = "German Shepherd"
 animal.speak = function () {
-    a="hey bro!!!";
-    return a;
+    return "hey bro!!!";
 }
 console.log(`Dog says ${animal.speak()}`)
