369966c83f46096d31ef873f9b16826512fc884f
Ivan <56585516+d3sreveR@users.noreply.github.com>
2019-12-26 20:08:13 +0100

    Objects

---
+++ Objects/script.js
@@ -0,0 +1,8 @@
+animal = new Object ();
+animal.name = "Doggo";
+animal.kind = "German Shepherd"
+animal.speak = function () {
+    a="hey bro!!!";
+    return a;
+}
+console.log(`Dog says ${animal.speak()}`)
